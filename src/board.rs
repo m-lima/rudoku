7bf191269950f5faa57e6c475a599373a64d81e8
Marcelo Lima <marcelowind@gmail.com>
2020-08-02 19:34:41 +0200

    Cluster iter

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,120 @@
+#[derive(Copy, Clone)]
 pub struct Board([u8; 81]);
+
+impl Board {
+    pub fn new() -> Self {
+        Self([0; 81])
+    }
+
+    pub fn initialize() -> Self {
+        let board = Self([
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9, 1, 2, 3, 7, 8, 9, 1, 2, 3, 4, 5, 6, 2, 3,
+            4, 5, 6, 7, 8, 9, 1, 5, 6, 7, 8, 9, 1, 2, 3, 4, 8, 9, 1, 2, 3, 4, 5, 6, 7, 3, 4, 5, 6,
+            7, 8, 9, 1, 2, 6, 7, 8, 9, 1, 2, 3, 4, 5, 9, 1, 2, 3, 4, 5, 6, 7, 8,
+        ]);
+        board
+    }
+
+    fn get(&self, coordinate: Coordinate) -> u8 {
+        self.0[coordinate.as_linear()]
+    }
+
+    fn set(&mut self, coordinate: Coordinate, value: u8) {
+        if value < 1 || value > 9 {
+            panic!("Invalid value: {}", value);
+        }
+        self.0[coordinate.as_linear()] = value;
+    }
+
+    fn clear(&mut self, coordinate: Coordinate) {
+        self.0[coordinate.as_linear()] = 0;
+    }
+
+    fn consistent(&self, coordinate: Coordinate) -> bool {
+        let reference = self.get(coordinate);
+        if reference == 0 {
+            return true;
+        }
+
+        for (index, value) in self.0.row(coordinate.row).enumerate() {
+            if index == usize::from(coordinate.row) {
+                continue;
+            }
+
+            if value == reference {
+                return false;
+            }
+        }
+
+        for (index, value) in self.0.column(coordinate.col).enumerate() {
+            if index == usize::from(coordinate.col) {
+                continue;
+            }
+
+            if value == reference {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    // fn list_inconsistencies(&self) -> Vec<Coordinate> {
+    //     vec![]
+    // }
+
+    fn reverse(&mut self) {
+        let temp = self.0.clone();
+        for i in 0..self.0.len() {
+            self.0[i] = temp[temp.len() - i - 1];
+        }
+    }
+
+    fn rotate(&mut self) {
+        let other = self.0.clone();
+        for i in 0..9 {
+            for (index, value) in other.column(i).enumerate() {
+                self.0[usize::from(i) * 9 + index] = value;
+            }
+        }
+    }
+
+    fn mirror_columns(&mut self) {
+        let other = self.0.clone();
+        for i in 0..9 {
+            for (index, value) in other.column(i).enumerate() {
+                self.0[index * 9 + (8 - usize::from(i))] = value;
+            }
+        }
+    }
+
+    fn mirror_rows(&mut self) {
+        let other = self.0.clone();
+        for i in 0..9 {
+            for (index, value) in other.row(i).enumerate() {
+                self.0[(8 - usize::from(i)) * 9 + index] = value;
+            }
+        }
+    }
+
+    // fn swap_rows(&mut self,
+    // swapRows(cluster: number, pivot: number) {
+    //   this.swapRowsByIndex(((pivot + 1) % 3) + cluster * 3, ((pivot + 2) % 3) + cluster * 3)
+    // }
+
+    // swapColumns(cluster: number, pivot: number) {
+    //   this.swapColumnsByIndex(((pivot + 1) % 3) + cluster * 3, ((pivot + 2) % 3) + cluster * 3)
+    // }
+}
+
+impl std::fmt::Debug for Board {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for i in 0..81 {
+            write!(fmt, "{},", self.0[i])?;
+        }
+        Ok(())
+    }
+}
 
 struct RowIterator<'a> {
     board: &'a [u8; 81],
@@ -26,6 +142,9 @@ trait IntoRowIterator {
 
 impl IntoRowIterator for [u8; 81] {
     fn row<'a>(&'a self, index: u8) -> RowIterator<'a> {
+        if index > 8 {
+            panic!("Invalid row: {}", index);
+        }
         RowIterator {
             board: &self,
             base: usize::from(index),
@@ -60,6 +179,9 @@ trait IntoColumnIterator {
 
 impl IntoColumnIterator for [u8; 81] {
     fn column<'a>(&'a self, index: u8) -> ColumnIterator<'a> {
+        if index > 8 {
+            panic!("Invalid column: {}", index);
+        }
         ColumnIterator {
             board: &self,
             base: usize::from(index),
@@ -68,6 +190,47 @@ impl IntoColumnIterator for [u8; 81] {
     }
 }
 
+struct ClusterIterator<'a> {
+    board: &'a [u8; 81],
+    base: usize,
+    index: usize,
+}
+
+impl std::iter::Iterator for ClusterIterator<'_> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < 9 {
+            let value = Some(self.board[self.base + ((self.index / 3) * 9) + (self.index % 3)]);
+            self.index += 1;
+            value
+        } else {
+            None
+        }
+    }
+}
+
+trait IntoClusterIterator {
+    fn cluster<'a>(&'a self, index: u8) -> ClusterIterator<'a>;
+}
+
+impl IntoClusterIterator for [u8; 81] {
+    fn cluster<'a>(&'a self, index: u8) -> ClusterIterator<'a> {
+        if index > 8 {
+            panic!("Invalid cluster: {}", index);
+        }
+        let col = 3 * (index % 3);
+        let row = 3 * (index / 3);
+        println!("{}", row * 9 + col);
+        ClusterIterator {
+            board: &self,
+            base: usize::from(row * 9 + col),
+            index: 0,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
 pub struct Coordinate {
     row: u8,
     col: u8,
@@ -75,104 +238,16 @@ pub struct Coordinate {
 
 impl Coordinate {
     fn as_linear(&self) -> usize {
-        if self.row > 9 || self.col > 9 {
+        if self.row > 8 || self.col > 8 {
             panic!("Cannot linearize (row: {}, col: {}", self.row, self.col);
         }
         usize::from(self.row * 9 + self.col)
     }
 }
 
-impl Board {
-    pub fn new() -> Self {
-        Self([0; 81])
-    }
-
-    pub fn initialize() -> Self {
-        let board = Self([
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9, 1, 2, 3, 7, 8, 9, 1, 2, 3, 4, 5, 6, 2, 3,
-            4, 5, 6, 7, 8, 9, 1, 5, 6, 7, 8, 9, 1, 2, 3, 4, 8, 9, 1, 2, 3, 4, 5, 6, 7, 3, 4, 5, 6,
-            7, 8, 9, 1, 2, 6, 7, 8, 9, 1, 2, 3, 4, 5, 9, 1, 2, 3, 4, 5, 6, 7, 8,
-        ]);
-        board
-    }
-
-    fn get(&self, coordinate: Coordinate) -> u8 {
-        self.0[coordinate.as_linear()]
-    }
-
-    fn set(&mut self, coordinate: Coordinate, value: u8) {
-        if value < 1 || value > 9 {
-            panic!("Invalid value: {}", value);
-        }
-        self.0[coordinate.as_linear()] = value;
-    }
-
-    fn clear(&mut self, coordinate: Coordinate) {
-        self.0[coordinate.as_linear()] = 0;
-    }
-
-    // fn consistent(&self, coordinate: Coordinate) -> bool {
-    //     if self.get(coordinate) == 0 {
-    //         return true;
-    //     }
-
-    //     let iter = RowIterator::new(
-    // }
-
-    // fn list_inconsistencies(&self) -> Vec<Coordinate> {
-    //     vec![]
-    // }
-
-    fn reverse(&mut self) {
-        let temp = self.0.clone();
-        for i in 0..self.0.len() {
-            self.0[i] = temp[temp.len() - i - 1];
-        }
-    }
-
-    fn rotate(&mut self) {
-        let other = self.0.clone();
-        for i in 0..9 {
-            let iter = other.column(i);
-            for (index, value) in iter.enumerate() {
-                self.0[usize::from(i) * 9 + index] = value;
-            }
-        }
-    }
-
-    fn mirror_columns(&mut self) {
-        let other = self.0.clone();
-        for i in 0..9 {
-            let iter = other.column(i);
-            for (index, value) in iter.enumerate() {
-                self.0[index * 9 + (8 - usize::from(i))] = value;
-            }
-        }
-    }
-
-    fn mirror_rows(&mut self) {
-        let other = self.0.clone();
-        for i in 0..9 {
-            let iter = other.row(i);
-            for (index, value) in iter.enumerate() {
-                self.0[(8 - usize::from(i)) * 9 + index] = value;
-            }
-        }
-    }
-
-    // fn swap_rows(&mut self,
-    // swapRows(cluster: number, pivot: number) {
-    //   this.swapRowsByIndex(((pivot + 1) % 3) + cluster * 3, ((pivot + 2) % 3) + cluster * 3)
-    // }
-
-    // swapColumns(cluster: number, pivot: number) {
-    //   this.swapColumnsByIndex(((pivot + 1) % 3) + cluster * 3, ((pivot + 2) % 3) + cluster * 3)
-    // }
-}
-
 #[cfg(test)]
 mod test {
-    use super::{Board, Coordinate, IntoColumnIterator, IntoRowIterator};
+    use super::{Board, Coordinate, IntoClusterIterator, IntoColumnIterator, IntoRowIterator};
 
     fn sequential_board() -> Board {
         let mut board = [0; 81];
@@ -291,6 +366,21 @@ mod test {
             let iter = board.0.column(i);
             for (index, value) in iter.enumerate() {
                 assert_eq!(value, index as u8 * 9 + i);
+            }
+        }
+    }
+
+    #[test]
+    fn cluster_iterator() {
+        let board = sequential_board();
+
+        for i in 0..9 {
+            let base = (27 * (i / 3)) + 3 * (i % 3);
+            let iter = board.0.cluster(i);
+            for (index, value) in iter.enumerate() {
+                let byte_index = index as u8;
+                let expected = base + (9 * (byte_index / 3)) + (byte_index % 3);
+                assert_eq!(value, expected);
             }
         }
     }
